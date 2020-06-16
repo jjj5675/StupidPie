f86aabec237c3d3cb8815b9bcf7c5af098b934a4
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-06-16 18:24:06 +0900

    jumppad, dash수정

--- Assets/Scripts/Character/StateMachineBehaviours/LocomotionSMB.cs
+++ Assets/Scripts/Character/StateMachineBehaviours/LocomotionSMB.cs
@@ -13,8 +13,9 @@ public class LocomotionSMB : SceneLinkedSMB<PlayerBehaviour>
         if (m_MonoBehaviour.CheckForUseableXAxis())
         {
             m_MonoBehaviour.UpdateFacing();
+            m_MonoBehaviour.GroundedHorizontalMovement();
         }
-        m_MonoBehaviour.GroundedHorizontalMovement();
+
         m_MonoBehaviour.GroundedVerticalMovement();
         if (m_MonoBehaviour.CheckForGrounded())
         {
@@ -24,8 +25,8 @@ public class LocomotionSMB : SceneLinkedSMB<PlayerBehaviour>
         {
             m_MonoBehaviour.SetVerticalMovement(m_MonoBehaviour.JumpVelocity);
         }
-        m_MonoBehaviour.CheckForGrabbingWall();
         m_MonoBehaviour.CheckForDashInput();
+        m_MonoBehaviour.CheckForGrabbingWall();
         m_MonoBehaviour.CheckForJumpPadCollisionEnter();
     }
 
