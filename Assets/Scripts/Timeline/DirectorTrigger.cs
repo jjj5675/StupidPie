8d469eb89d00f4c624909cc6fbfe29050b2afefa
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-07-03 01:41:07 +0900

    Update Sample Timeline

---
+++ Assets/Scripts/Timeline/DirectorTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(Collider2D))]
+public class DirectorTrigger : MonoBehaviour
+{
+    public List<GameObject> triggeringGameObject;
+    public PlayableDirector director;
+    public UnityEvent OnDirectorPlay;
+    public UnityEvent OnDirectorFinish;
+
+    protected bool m_AlreadTriggered;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!triggeringGameObject.Contains(collision.gameObject))
+        {
+            return;
+        }
+
+        if(m_AlreadTriggered)
+        {
+            return;
+        }
+
+        director.Play();
+        m_AlreadTriggered = true;
+        OnDirectorPlay.Invoke();
+        Invoke("FinishInvoke", (float)director.duration);
+    }
+
+    void FinishInvoke()
+    {
+        OnDirectorFinish.Invoke();
+    }
+
+}
