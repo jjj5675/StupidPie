f1bbeb5180d3cea5a0fdc6fe5aaa863e98ea1947
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-07-09 20:23:50 +0900

    Update Dialouge Canvas Controller

---
+++ Assets/Scripts/UI/DialogueCanvasController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueCanvasController : MonoBehaviour
+{
+    public Animator animator;
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI phrases;
+
+    public void 
+}
