bd71e92e6349af812b2a4f34f1b565ae7ab08792
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-07-09 22:14:02 +0900

    Update Dialogue Canvas Animation, script

--- Assets/Scripts/UI/DialogueCanvasController.cs
+++ Assets/Scripts/UI/DialogueCanvasController.cs
@@ -6,8 +6,46 @@ using TMPro;
 public class DialogueCanvasController : MonoBehaviour
 {
     public Animator animator;
-    public TextMeshProUGUI name;
-    public TextMeshProUGUI phrases;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI mainText;
+    
+    private List<Phrase> m_GetPhrases = new List<Phrase>();
+    private readonly int m_HashActivePara = Animator.StringToHash("Active");
+    private Coroutine m_DeactiveCoroutine;
 
-    public void 
+    IEnumerator SetAnimatorParameterWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        animator.SetBool(m_HashActivePara, false);
+    }
+
+    public void ActivateCanvasWithTranslatedText(string phraseKey)
+    {
+        m_GetPhrases = DialogueManager.Instance[phraseKey];
+
+        if(m_GetPhrases == null)
+        {
+            Debug.LogError("Key not found.");
+            return;
+        }
+
+        if(m_DeactiveCoroutine != null)
+        {
+            StopCoroutine(m_DeactiveCoroutine);
+            m_DeactiveCoroutine = null;
+        }
+
+        gameObject.SetActive(true);
+        animator.SetBool(m_HashActivePara, true);
+
+        foreach(var phrase in m_GetPhrases)
+        {
+            print(phrase.name + " : " + phrase.value);
+        }
+    }
+
+    public void DeactivateCanvasWithDelay(float delay)
+    {
+        m_DeactiveCoroutine = StartCoroutine(SetAnimatorParameterWithDelay(delay));
+    }
 }
