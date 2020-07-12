7573e60fd73da366ed0f2220b1e45571100ca424
jjj5675 <48472223+jjj5675@users.noreply.github.com>
2020-07-12 23:22:05 +0900

    Update VFX ghost
    1. GhostPool
    2. Ghost
    3. Ghost Shader

---
+++ Assets/Scripts/Character/MonoBehaviours/Ghost.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    private SpriteRenderer m_SpriteRenderer;
+    private float m_EffectDuration;
+
+    private void Awake()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetGhost(float duration, SpriteRenderer referenceRenderer, Color effectColor, bool useColor, Transform referenceTransform)
+    {
+        m_SpriteRenderer.sprite = referenceRenderer.sprite;
+        m_SpriteRenderer.sortingLayerID = referenceRenderer.sortingLayerID;
+        m_SpriteRenderer.sortingOrder = referenceRenderer.sortingOrder - 1;
+        m_SpriteRenderer.flipX = referenceRenderer.flipX;
+
+        m_EffectDuration = duration;
+        transform.position = referenceTransform.position;
+        transform.rotation = referenceTransform.rotation;
+
+        if (useColor)
+        {
+            m_SpriteRenderer.material.SetColor("_Color", effectColor);
+            m_SpriteRenderer.color = effectColor;
+            StartCoroutine(DissapearSprite(useColor, m_SpriteRenderer.color));
+        }
+        else
+        {
+            StartCoroutine(DissapearSprite(useColor, m_SpriteRenderer.material.color, effectColor.a));
+        }
+    }
+
+    IEnumerator DissapearSprite(bool useColor, Color startColor, float startAlpha = 0f)
+    {
+        bool finishedLerping = false;
+        float startLerpTime = Time.time;
+
+        Color transparencyColor = new Color(0, 0, 0, 0);
+
+        while(!finishedLerping)
+        {
+            float timeSinceLerpStart = Time.time - startLerpTime;
+            float percentComplete = timeSinceLerpStart / m_EffectDuration;
+
+            if(1f <= percentComplete)
+            {
+                finishedLerping = true;
+
+                if (useColor)
+                {
+                    m_SpriteRenderer.material.SetColor("_Color", transparencyColor);
+                }
+                else
+                {
+                    startColor.a = 0;
+                    m_SpriteRenderer.color = startColor;
+                }
+            }
+
+            if (useColor)
+            {
+                Color newColor = Color.Lerp(startColor, transparencyColor, percentComplete);
+                m_SpriteRenderer.material.SetColor("_Color", newColor);
+            }
+            else
+            {
+                float newAlpha = Mathf.Lerp(startAlpha, 0, percentComplete);
+                startColor.a = newAlpha;
+                m_SpriteRenderer.color = startColor;
+            }
+
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Character/MonoBehaviours/GhostPool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostPool : MonoBehaviour
+{
+    public float effectDuration;
+    public float spawnRate;
+    //public int maxGhosts;
+    public GameObject prefab;
+    public bool useColor;
+    public Color effectColor;
+
+    private SpriteRenderer m_SpriteRenderer;
+    private int m_SortingLayer;
+    [SerializeField]
+    private bool m_Started = false;
+    private float m_CurrentSpawnTime;
+
+    private void Awake()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SortingLayer = m_SpriteRenderer.sortingLayerID;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!m_Started)
+        {
+            return;
+        }
+
+        m_CurrentSpawnTime += Time.deltaTime;
+
+        if(spawnRate <= m_CurrentSpawnTime)
+        {
+            GameObject ghost = Instantiate(prefab);
+            ghost.GetComponent<Ghost>().SetGhost(effectDuration, m_SpriteRenderer, effectColor, useColor, transform);
+            m_CurrentSpawnTime = 0f;
+        }
+    }
+}
